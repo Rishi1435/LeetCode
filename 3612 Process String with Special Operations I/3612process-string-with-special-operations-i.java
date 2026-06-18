123e11bb9bd72fc0b4d5de20581b2884915be372
Rishi1435 <pediredlarishi2005@gmail.com>
2026-06-18 09:38:31 +0000

    Code challenge submission for question: 3612 Process String with Special Operations I

---
+++ 3612 Process String with Special Operations I/3612process-string-with-special-operations-i.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String processStr(String s) {
+        StringBuilder sb=new StringBuilder();
+        for(char ch:s.toCharArray()){
+            if(ch=='*'){
+                if(sb.length()<=0){
+                    continue;
+                }else{
+                sb.deleteCharAt(sb.length()-1);
+                }
+            }
+            else if(ch=='#'){
+                sb.append(sb);
+            }
+            else if(ch=='%'){
+                sb.reverse();
+            }
+            else{
+                sb.append(ch);
+            }
+        }
+        return sb.toString();
+        
+    }
+}
