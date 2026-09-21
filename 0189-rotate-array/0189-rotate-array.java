080eaa2ef60cd7ab35e30dfeecdc636015ee2447
P Vishnu Venkata Rishi <23mh1a05o1@acoe.edu.in>
2026-09-21 12:59:28 +0530

    Time: 5 ms (29.3%), Space: 268.3 MB (53.51%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n=nums.length;
+        k=k%n;
+        reverseArray(nums,0,n-1);
+        reverseArray(nums,0,k-1);
+        reverseArray(nums,k,n-1);
+    }
+    private void reverseArray(int[] nums,int left,int right){
+        while(left<right){
+            int temp=nums[left];
+            nums[left]=nums[right];
+            nums[right]=temp;
+            left++;
+            right--;
+        }
+    }
+}
